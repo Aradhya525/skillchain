f3dc90c28853d160115c332cefbff6d0f4a6a0a3
ARADHYA NEGI <an8038@srmist.edu.in>
2026-03-20 20:37:44 +0530

    Add artifacts/skillchain/src/lib/utils.ts

---
+++ artifacts/skillchain/src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// Generate beautiful consistent avatar seeds based on username
+export function getAvatarUrl(name: string) {
+  return `https://api.dicebear.com/7.x/avataaars/svg?seed=${encodeURIComponent(name)}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffdfbf,ffd5dc`;
+}
